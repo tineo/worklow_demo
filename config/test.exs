b7a54a1871834b3ab1bf329367e11a9979fae7cb
Cesar Gutierrez Tineo <a20062161@pucp.edu.pe>
2020-07-23 21:07:13 -0500

    fix test config

--- config/test.exs
+++ config/test.exs
@@ -12,7 +12,6 @@ config :workflow_demo, WorkflowDemo.Repo,
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
-
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :workflow_demo, WorkflowDemoWeb.Endpoint,
